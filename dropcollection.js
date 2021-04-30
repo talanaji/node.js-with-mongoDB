3a8776eb1a38643f4d2eddb258f753e6c63a805b
talanaji <talanaji@gmail.com>
2021-04-30 04:29:52 +0200

    drop collection

---
+++ dropcollection.js
@@ -0,0 +1,14 @@
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017/";
+
+MongoClient.connect(url, function(err, db) {
+  if (err) throw err;
+  var dbo = db.db("mydb");
+  //you can user drop() or dropCollection() to drop collection
+  //  dbo.dropCollection("customers", function(err, delOK) {
+  dbo.collection("customers").drop(function(err, delOK) {
+    if (err) throw err;
+    if (delOK) console.log("Collection deleted");
+    db.close();
+  });
+});
