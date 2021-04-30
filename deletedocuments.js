d23e06df42b17a0354728912455fb872cf9b5cf5
talanaji <talanaji@gmail.com>
2021-04-30 04:18:38 +0200

    delete documents

---
+++ deletedocuments.js
@@ -0,0 +1,25 @@
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017/";
+
+MongoClient.connect(url, function(err, db) {
+  if (err) throw err;
+  var dbo = db.db("mydb");
+  //Delete the document with the address "Mountain 21":
+
+
+  var myquery = { address: 'Mountain 21' };
+  dbo.collection("customers").deleteOne(myquery, function(err, obj) {
+    if (err) throw err;
+    console.log("1 document deleted");
+    db.close();
+  });
+  //Delete all documents were the address starts with the letter "O":
+
+
+   var myquery = { address: /^O/ };
+  dbo.collection("customers").deleteMany(myquery, function(err, obj) {
+    if (err) throw err;
+    console.log(obj.result.n + " document(s) deleted");
+    db.close();
+  });
+});
